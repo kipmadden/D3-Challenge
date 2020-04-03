59cfe475b45dabc48c4165f0e67edc55af2256e6
kipmadden <56366836+kipmadden@users.noreply.github.com>
2020-04-03 18:08:04 -0500

    csv read code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
 // @TODO: YOUR CODE HERE!
+// Load data from data.csv
+d3.csv("data.csv").then(function(demographic) {
+    
+    console.log(demographic);
+  
+    // // log a list of id
+    // var ids = demographic.map(data => data.id);
+    // console.log("id", ids);
+  
+    // // Cast each hours value in demographic as a number using the unary + operator
+    // demographic.forEach(function(data) {
+    //   data.poverty = +data.poverty;
+    //   console.log("id:", data.id);
+    //   console.log("poverty:", data.poverty);
+    // });
+  });
