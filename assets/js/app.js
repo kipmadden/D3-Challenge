6e279100be05e4d9cb4bdfc5e351e9f50b6f7be7
kipmadden <56366836+kipmadden@users.noreply.github.com>
2020-04-03 21:35:28 -0500

    minor mods

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,19 @@
 // @TODO: YOUR CODE HERE!
 // Load data from data.csv
-d3.csv("data.csv").then(function(demographic) {
+d3.csv("assets/data/data.csv", d3.autoType).then(function(demographic) {
     
     console.log(demographic);
   
-    // // log a list of id
-    // var ids = demographic.map(data => data.id);
-    // console.log("id", ids);
+    // log a list of id
+    var ids = demographic.map(data => data.id);
+    console.log("id", ids);
   
-    // // Cast each hours value in demographic as a number using the unary + operator
-    // demographic.forEach(function(data) {
-    //   data.poverty = +data.poverty;
+    // Cast each hours value in demographic as a number using the unary + operator
+    demographic.forEach(function(data) {
+      data.poverty = +data.poverty;
     //   console.log("id:", data.id);
     //   console.log("poverty:", data.poverty);
-    // });
+    });
   });
+// const demoData = d3.csv("./assets/data/data.csv", d3.autoType );
+// console.log(demoData);
