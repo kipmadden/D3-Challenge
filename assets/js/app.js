02770deca60f1e0e88f594a2403261030d2e2d6a
kipmadden <56366836+kipmadden@users.noreply.github.com>
2020-04-04 19:54:22 -0500

    Completed required code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,117 @@
 // @TODO: YOUR CODE HERE!
-// Load data from data.csv
+var svgWidth = 800;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+/* Load data from data.csv and use d3.autoType to automatically cast all 
+strings to numbers in one shot */
 d3.csv("assets/data/data.csv", d3.autoType).then(function(demographic) {
     
     console.log(demographic);
   
-    // log a list of id
-    var ids = demographic.map(data => data.id);
-    console.log("id", ids);
-  
-    // Cast each hours value in demographic as a number using the unary + operator
-    demographic.forEach(function(data) {
-      data.poverty = +data.poverty;
-    //   console.log("id:", data.id);
-    //   console.log("poverty:", data.poverty);
-    });
+   // Step 1: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([8, d3.max(demographic, d => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([4, d3.max(demographic, d => d.healthcare)])
+      .range([height, 0]);
+
+    // Step 2: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 3: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 4: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup
+      .append("g")
+      .selectAll(".circle")
+      .data(demographic)
+      .enter()
+      .append("g")
+      .classed('circle', true)
+      .attr('transform', d => `translate(${xLinearScale(d.poverty)},${yLinearScale(d.healthcare)})`);
+
+
+    circlesGroup
+      .append("circle")
+      .attr("r", "10")
+      .attr("fill", "blue")
+      .attr("opacity", ".9");
+
+    circlesGroup
+      .append('text')
+      .classed('circleText', true)
+      .attr('dy', '0.35em')
+      .attr('dx', -6)
+      .text(d => d.abbr);
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.abbr}<br>In Poverty %: ${d.poverty}<br>Lacks Healthcare %: ${d.healthcare}`);
+      });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
+
   });
-// const demoData = d3.csv("./assets/data/data.csv", d3.autoType );
-// console.log(demoData);
